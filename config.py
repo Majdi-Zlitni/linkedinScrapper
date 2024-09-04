cf49faf9b12905df8e7bfd623502649ec2aa2d91
Majdi Zlitni <majdiuchiha@gmail.com>
2024-09-04 15:22:06 +0100

    remove file

--- config.py
+++
@@ -1,6 +0,0 @@
-# LinkedIn login credentials
-class Configuration:
-    email = "cajit16118@obisims.com"
-    password = "TestTest#2024"
-    linkedInFeedURL="https://www.linkedin.com/feed/"
-    linkedInURLGroupURL="https://www.linkedin.com/groups/1772532/"
